56a9d1a1b34b0e1ed28ab971e95ecdaa1584eb4b
MichaelJahns <jahnsmic001@gmail.com>
2019-09-08 10:58:00 -0700

    Links stylesheets, makes some quality of life adjustments, and roughly plans dash

---
+++ src/main/java/michaelj/namespace/namespace/herbology/Herb.java
@@ -0,0 +1,17 @@
+package michaelj.namespace.namespace.herbology;
+
+import javax.persistence.Entity;
+
+import static michaelj.namespace.namespace.board.Dice.rollDice;
+
+//@Entity
+public class Herb {
+    public static void main (String args[]){
+        System.out.println("Critical find");
+
+        rollDice(12, 2, false, false);
+    }
+
+    public static
+
+}
