c2fa59dbc5696ce2b62b5eb3882732f50f2c5672
MichaelJahns <jahnsmic001@gmail.com>
2019-10-09 20:36:16 -0700

    Allows a userAccount to hold a list of campaigns they DM, with the aims of players being able to tether themselves to campaigns, and to be able to navigate the site using object ids

--- src/main/java/michaelj/namespace/namespace/campaign/CampaignController.java
+++ src/main/java/michaelj/namespace/namespace/campaign/CampaignController.java
@@ -41,14 +41,19 @@ public class CampaignController {
             Principal p,
             Model model,
             @RequestParam String campaignMoniker,
-            @RequestParam String campaignWorld
+            @RequestParam String campaignWorld,
+            @RequestParam(required = false) Boolean isUserDM
     ){
         UserAccount user = this.accountRepo.findByUsername(p.getName());
+        Campaign newCampaign;
+        if(isUserDM){
+            newCampaign = new Campaign(user, campaignMoniker, campaignWorld, user);
+        }else{
+            newCampaign = new Campaign(user, campaignMoniker, campaignWorld);
 
-        Campaign newCampaign = new Campaign(campaignMoniker, campaignWorld, user);
-        campaignRepo.save(newCampaign);
+        }
         user.addCampaign(newCampaign);
-
+        campaignRepo.save(newCampaign);
         accountRepo.save(user);
 
         return "redirect:/campaign";
