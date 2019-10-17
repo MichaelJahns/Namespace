800ee9983467ac9bd0e4c5986757230bca16ed68
MichaelJahns <jahnsmic001@gmail.com>
2019-10-17 13:34:54 -0700

    explores weighted graph to capture relationship between characters

--- src/main/java/michaelj/namespace/namespace/tester.java
+++ src/main/java/michaelj/namespace/namespace/tester.java
@@ -1,7 +1,22 @@
 package michaelj.namespace.namespace;
 
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
 public class tester {
     public static void main(String args[]) {
+        int vertices = 6;
+        WeightedGraph graph = new WeightedGraph(vertices);
+        graph.addEdge(0, 1, 4);
+        graph.addEdge(0, 2, 3);
+        graph.addEdge(1, 3, 2);
+        graph.addEdge(1, 2, 5);
+        graph.addEdge(2, 3, 7);
+        graph.addEdge(3, 4, 2);
+        graph.addEdge(4, 0, 4);
+        graph.addEdge(4, 1, 4);
+        graph.addEdge(4, 5, 6);
+        graph.printGraph();
+
     }
 }
 
